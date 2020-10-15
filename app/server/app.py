a60fe125b0c34848dac2226244470ac24029dae7
angmar33 <paultsvilyuk@gmail.com>
2020-10-15 22:03:47 +0300

    motor added to interact with mongo asynchronously. added routes to create/update/delete/view students.

--- app/server/app.py
+++ app/server/app.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
+from server.routes.student import router as student_router
+
+
 app = FastAPI()
+
+
+app.include_router(student_router, tags=["Student"], prefix="/student")
 
 
 @app.get("/", tags=["Root"])
